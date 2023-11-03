8991101796b8787ad902109f3d17e8b56c224dd4
Shin Dongchan <chanshin0@github.com>
2023-11-03 17:57:37 +0900

    init pjt 1. 양옆 레이아웃 및 바텀 내브바 영역 설정 2. 라이브러리 설치

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main 
+    className='
+    w-full h-full bg-orange-300 flex flex-col items-center 
+    '
+    >
+      main contents here
+      <footer className="
+      fixed bottom-0 
+      ">
+        bottom navBar here
+      </footer>
+    </main>
+  )
+}
